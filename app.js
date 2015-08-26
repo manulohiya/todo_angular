fcb14117172a7f29aa838ebf8e082fd29740cb5e
Manu Lohiya <manulohiya@gmail.com>
2015-08-25 17:30:07 -0700

    added directive

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 angular.module('todoApp', ["ngRoute"])
 
-.config(['$routeProvider',
-   function($routeProvider) {
+.config(['$routeProvider', '$locationProvider',
+   function($routeProvider, $locationProvider) {
      $routeProvider.
        when('/', {
          templateUrl: 'templates/todos.html',
          controller: 'TodoCtrl'
        }).
+       when('/about', {
+       	templateUrl: 'templates/about.html'
+         
+     }).
+       when('/lists', {
+       	templateUrl: 'templates/lists.html',
+       	controller: 'ListCtrl'
+         
+     }).
        otherwise({
          redirectTo: '/'
        });
+
+       // use the HTML5 History API for pretty URLs. Doesnt let you refresh.
+        $locationProvider.html5Mode({
+          enabled: true,
+          requireBase: false
+        });
+
    }])
+
+
+
+
 
 .controller("MainCtrl", ['$scope', '$rootScope', function ($scope, $rootScope) {
   $scope.term = "cellar door";	
@@ -20,10 +40,10 @@ angular.module('todoApp', ["ngRoute"])
 }])
 
 .controller("TodoCtrl", ['$scope', function ($scope) {
-	console.log("Chaining is working");
+	console.log("Chaining is working on Todo");
 
 $scope.todos = [
-{title: "clean", created_at: 1288323623006},
+{title: "clean"},
 {title: "sleep"}
 ];
 
@@ -34,7 +54,7 @@ $scope.delete = function(todo) {
 }	
 $scope.newTodo = "Enter task";
 $scope.addNewTodo = function() {
- 	$scope.todos.push({title: $scope.newTodo,created_at: new Date()});
+ 	$scope.todos.push({title: $scope.newTodo, created_at: new Date()});
  	$scope.newTodo = " "
 
 
@@ -44,6 +64,48 @@ $scope.addNewTodo = function() {
 
   
 }])
+
+
+.controller("ListCtrl", ['$scope', function ($scope) {
+	console.log("Chaining is working on Lists");
+
+$scope.lists = [
+{title: "work"},
+{title: "family"}
+];
+
+
+ 
+}])
+
+
+.directive('currentWeather', function() {
+	console.log("currentWeather directive is working");
+  return {
+    restrict: 'E',
+    scope: {
+      city: '@'
+    },
+    template: '<div class="current-weather"><h4>Weather for {{city}}</h4>{{weather.main.temp}}</div>',
+    //tempalteUrl: 'templates/current-weather-template.html',
+    //transclude: true,
+    controller: ['$scope', '$http',
+      function ($scope, $http) {
+        var url = "http://api.openweathermap.org/data/2.5/weather?mode=json&cnt=7&units=imperial&callback=JSON_CALLBACK&q="
+        $scope.getWeather = function(city) {
+          $http({ method: 'JSONP', url: url + city })
+            .success(function(data) {
+              $scope.weather = data;
+            });
+        }
+    }],
+    link: function (scope, element, attrs) {
+      scope.weather = scope.getWeather(attrs.city);
+    }
+  }
+});
+
+
 
 
 
